3c33e11b69386299b9182caacb2090c11895b8d3
OP-jerbe <j.erbe@oregon-physics.com>
2025-06-16 13:46:17 -0700

    Adds a print statement to the __init__ method saying the HEU class was created.

--- src/heu3/heu3_api.py
+++ src/heu3/heu3_api.py
@@ -3,4 +3,9 @@ class HEUv3:
     Provides the api for serial communication with the HEUv3.
     """
 
-    def __init__(self) -> None: ...
+    def __init__(self) -> None:
+        print('You have created an HEUv3 class')
+
+
+if __name__ == '__main__':
+    heu = HEUv3()
